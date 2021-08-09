f1f52230f9422bd3bd2d423da1f1d14fa8f36ebe
aleksey <lifedok@gmail.com>
2021-08-10 01:15:26 +0300

    Add dynamic header

--- src/views/header/trip-info-header.js
+++ src/views/header/trip-info-header.js
@@ -5,7 +5,7 @@ export const createTripInfoHeaderTemplate = (listEvent) => {
   return (
     `<section class="trip-main__trip-info  trip-info">
       <div class="trip-info__main">
-        <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+        <h1 class="trip-info__title">${listEvent[0].destination.city} &mdash; ${listEvent[Math.floor(listEvent.length / 2)].destination.city} &mdash; ${listEvent[listEvent.length-1].destination.city}</h1>
 
         <p class="trip-info__dates">${listEvent[0].dateFrom.format('MMM D')}&nbsp;&mdash;&nbsp;${listEvent[listEvent.length-1].dateTo.format('D')}</p>
       </div>
