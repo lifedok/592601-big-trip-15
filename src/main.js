65210963acf983a691c98d173a9103e98bef3cc6
aleksey <lifedok@gmail.com>
2021-08-19 23:38:38 +0300

    Moved logic to presenter

--- src/main.js
+++ src/main.js
@@ -1,91 +1,18 @@
-// header
-import TripInfoHeaderView from './views/header/trip-info-header.js';
-import TripControlsWrapperView from './views/header/trip-controls-wrapper.js';
-import TripTabsHeaderView from './views/header/trip-tabs-header.js';
-import TripFiltersHeaderView from './views/header/trip-filters-header';
-import NewEventButtonView from './views/header/new-event-button.js';
-
-// main
-import TripEventSortView from './views/trip-event-sort.js';
-import TripEventListWrapperView from './views/trip-event-list-wrapper.js';
-import TripEventModifyItemView from './views/trip-event-modify-item.js';
-import TripEventItemView from './views/trip-event-item.js';
-
-// warning message
-import TripEventMsgView from './views/trip-event-msg.js';
-
 
 // generate mock data
 import {generateTripEventListData} from './mock/trip-event-list-data.js';
-import {render, replace} from './utils/render';
+import TripPresenter from './presenter/trip';
 
 
 const COUNT_ITEMS = 9;
 const tripEventList = generateTripEventListData(COUNT_ITEMS);
 tripEventList.sort((a, b) => a.dateFrom - b.dateFrom);
 
+
 const pageBodyElement = document.querySelector('.page-body');
+const tripEventsHeader = pageBodyElement.querySelector('.trip-main');
+const tripEventsMainContainer = pageBodyElement.querySelector('.trip-events');
 
 
-// create header
-const tripMainHeader = pageBodyElement.querySelector('.trip-main');
-render(tripMainHeader, new TripInfoHeaderView(tripEventList));
-
-const ControlsWrapper = new TripControlsWrapperView();
-render(tripMainHeader, ControlsWrapper);
-render(ControlsWrapper, new TripTabsHeaderView());
-render(ControlsWrapper, new TripFiltersHeaderView());
-render(tripMainHeader, new NewEventButtonView());
-
-// // create main content
-const tripEvents = pageBodyElement.querySelector('.trip-events');
-render(tripEvents, new TripEventSortView());
-const listWrapper = new TripEventListWrapperView();
-render(tripEvents, listWrapper);
-
-const renderTripEvent = (tripEventListElement, tripEvent) => {
-  const tripEventComponent = new TripEventItemView(tripEvent);
-  const tripEventEditComponent = new TripEventModifyItemView(tripEvent, true);
-
-  const replaceTripToEditForm = () => {
-    replace(tripEventEditComponent, tripEventComponent);
-  };
-
-  const replaceFormToItem = () => {
-    replace(tripEventComponent, tripEventEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === 'Escape' || evt.key === 'Esc') {
-      evt.preventDefault();
-      replaceFormToItem();
-      document.removeEventListener('keydown', onEscKeyDown);
-    }
-  };
-
-  const closeTripEventEditView = () => {
-    replaceFormToItem();
-    document.removeEventListener('keydown', onEscKeyDown);
-  };
-
-  tripEventComponent.setOpenClickHandler(() => {
-    replaceTripToEditForm();
-    document.addEventListener('keydown', onEscKeyDown);
-  });
-
-  tripEventEditComponent.setCloseClickHandler(() => closeTripEventEditView());
-  tripEventEditComponent.setResetClickHandler(() => closeTripEventEditView());
-  tripEventEditComponent.setFormSubmitHandler(() => closeTripEventEditView());
-  tripEventEditComponent.setSaveClickHandler(() => closeTripEventEditView());
-
-  render(tripEventListElement, tripEventComponent);
-};
-
-
-if (tripEventList.length) {
-  tripEventList.map((item) =>
-    renderTripEvent(listWrapper, item),
-  );
-} else {
-  render(tripEvents, new TripEventMsgView('Click New Event to create your first point'));
-}
+const tripPresenter = new TripPresenter(tripEventsHeader, tripEventsMainContainer);
+tripPresenter.init(tripEventList);
--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,50 +1,113 @@
+// header
+import TripInfoHeaderView from '../views/header/trip-info-header.js';
+import TripControlsWrapperView from '../views/header/trip-controls-wrapper.js';
+import TripTabsHeaderView from '../views/header/trip-tabs-header.js';
+import TripFiltersHeaderView from '../views/header/trip-filters-header';
+import NewEventButtonView from '../views/header/new-event-button.js';
+
+// main
 import TripEventSortView from '../views/trip-event-sort';
 import TripEventListWrapperView from '../views/trip-event-list-wrapper';
 import TripEventModifyItemView from '../views/trip-event-modify-item';
 import TripEventItemView from '../views/trip-event-item';
+
+// warning message
 import TripEventMsgView from '../views/trip-event-msg';
+import {render, replace} from '../utils/render';
 
 
 export default class Trip {
-  constructor(tripContainer) {
-    this._tripContainer = tripContainer;
+  constructor(tripEventsHeaderView, tripEventsMainView) {
+    this._tripEventsHeaderView = tripEventsHeaderView;
+    this._tripEventsMainView = tripEventsMainView;
 
-    this._eventSortComponent = new TripEventSortView();
-    this._eventListComponent = new TripEventListWrapperView();
-    this._eventModifyComponent = new TripEventModifyItemView();
-    this._eventItemComponent = new TripEventItemView();
-    this._noEventComponent = new TripEventMsgView();
+    this._tripEventListWrapper = new TripEventListWrapperView();
   }
 
-  init(tripPoints) {
-    this._tripPoints = tripPoints.slice();
-    // Метод для инициализации (начала работы) модуля,
-    // малая часть текущей функции renderTrip в main.js
+  init(tripEventList) {
+    this._tripEventList = tripEventList;
+
+
+    this._renderHeader();
+    this._renderSort();
+
+    render(this._tripEventsMainView, this._tripEventListWrapper);
+    this._renderTrip();
   }
 
   _renderSort() {
-    // Метод для рендеринга сортировки
+    render(this._tripEventsMainView, new TripEventSortView());
   }
 
   _renderFilter() {
-    // Метод для рендеринга фильтрации
-  }
-
-  _renderEvent() {
-    // Метод, куда уйдёт логика созданию и рендерингу компонетов задачи,
-    // текущая функция renderTripEvent в main.js
+    render(this._controlsWrapperView, new TripFiltersHeaderView());
   }
 
   _renderEvents() {
-    // Метод для рендеринга N-событий за раз
+    this._tripEventList.map((event) => this._renderEvent(event));
+  }
+
+  _renderEvent(event) {
+    const tripEventEditComponent = new TripEventModifyItemView(event, true);
+    const tripEventItemView = new TripEventItemView(event);
+
+    const replaceTripToEditForm = () => {
+      replace(tripEventEditComponent, tripEventItemView);
+    };
+
+    const replaceFormToItem = () => {
+      replace(tripEventItemView, tripEventEditComponent);
+    };
+
+    const onEscKeyDown = (evt) => {
+      if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        replaceFormToItem();
+        document.removeEventListener('keydown', onEscKeyDown);
+      }
+    };
+
+    const closeTripEventEditView = () => {
+      replaceFormToItem();
+      document.removeEventListener('keydown', onEscKeyDown);
+    };
+
+    tripEventItemView.setOpenClickHandler(() => {
+      replaceTripToEditForm();
+      document.addEventListener('keydown', onEscKeyDown);
+    });
+
+    tripEventEditComponent.setCloseClickHandler(() => closeTripEventEditView());
+    tripEventEditComponent.setResetClickHandler(() => closeTripEventEditView());
+    tripEventEditComponent.setFormSubmitHandler(() => closeTripEventEditView());
+    tripEventEditComponent.setSaveClickHandler(() => closeTripEventEditView());
+
+    render(this._tripEventListWrapper, tripEventItemView);
   }
 
   _renderNoEventTrip() {
-    // Метод для рендеринга заглушки
+    this._noEventComponent = new TripEventMsgView('Click New Event to create your first point');
+    render(this._tripEventListWrapper, this._noEventComponent);
+  }
+
+  _renderHeader() {
+    this._controlsWrapperView = new TripControlsWrapperView();
+    this._tripTabsHeaderView = new TripTabsHeaderView();
+
+
+    render(this._tripEventsHeaderView, new TripInfoHeaderView(this._tripEventList));
+    render(this._tripEventsHeaderView, this._controlsWrapperView);
+    render(this._controlsWrapperView, this._tripTabsHeaderView);
+    this._renderFilter();
+    render(this._tripEventsHeaderView, new NewEventButtonView());
   }
 
   _renderTrip() {
-    // Метод для инициализации (начала работы) модуля,
-    // бОльшая часть текущей функции renderTrip в main.js
+
+    if (!this._tripEventList.length) {
+      this._renderNoEventTrip();
+    } else {
+      this._renderEvents();
+    }
   }
 }
