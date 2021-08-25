b5ed62c644fb0fd18b963d378a4b2385edf9653c
aleksey <lifedok@gmail.com>
2021-08-25 20:56:31 +0300

    Simplify for sortPointsByPrice

--- src/utils/point.js
+++ src/utils/point.js
@@ -4,14 +4,4 @@ export const sortPointsByDay = (pointA, pointB) => (
   dayjs(pointA.dateFrom).diff(dayjs(pointB.dateFrom))
 );
 
-export const sortPointsByPrice = (pointA, pointB) => {
-  if (pointA.basePrice > pointB.basePrice) {
-    return -1;
-  }
-  if (pointA.basePrice < pointB.basePrice) {
-    return 1;
-  }
-  if (pointA.basePrice === pointB.basePrice) {
-    return 0;
-  }
-};
+export const sortPointsByPrice = (pointA, pointB) => pointB.basePrice - pointA.basePrice;
