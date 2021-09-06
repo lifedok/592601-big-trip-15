87cacdbf2f73d123e8ad4e8268d5c8ee0edea7ca
aleksey <lifedok@gmail.com>
2021-09-06 19:13:34 +0300

    Added type = number for price

--- src/views/point-item-modify.js
+++ src/views/point-item-modify.js
@@ -115,7 +115,7 @@ const createPointItemModifyTemplate = (data, isEdit) => {
             <span class="visually-hidden">Price</span>
             &euro;
           </label>
-          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${!data ? '' : data.basePrice}">
+          <input class="event__input  event__input--price" id="event-price-1" type="number" name="event-price" value="${!data ? '' : data.basePrice}">
         </div>
 
         <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
