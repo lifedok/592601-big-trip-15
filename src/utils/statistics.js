b62a49f295a82b0156b279c3014280f3ff689fc1
aleksey <lifedok@gmail.com>
2021-09-14 01:01:55 +0300

    Refactor & clean up

--- src/utils/statistics.js
+++ src/utils/statistics.js
@@ -1,3 +1,7 @@
+import Chart from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+
 export const makeItemsUniq = (items) => [...new Set(items)];
 
 export const countPointsByType = (points, type) => points.filter((point) => point.type === type).length;
@@ -10,3 +14,82 @@ export function getTotalPrice(pointByTypes) {
   pointByTypes.map((pointType) => total += pointType.price);
   return total;
 }
+
+
+export const getGraphChart = (
+  context,   // htmlElement
+  labels,    // array
+  data,      // array
+  titleText, // string
+  formatter, // string
+  carency,   // boolean
+) => {
+  const getFormatter = (val) => carency ? val + formatter : formatter + val;
+  return (
+    new Chart(context, {
+      plugins: [ChartDataLabels],
+      type: 'horizontalBar',
+      data: {
+        labels: labels,
+        datasets: [{
+          data: data,
+          backgroundColor: '#ffffff',
+          hoverBackgroundColor: '#ffffff',
+          anchor: 'start',
+        }],
+      },
+      options: {
+        plugins: {
+          datalabels: {
+            font: {
+              size: 13,
+            },
+            color: '#000000',
+            anchor: 'end',
+            align: 'start',
+            formatter: (val) => getFormatter(val),
+          },
+        },
+        title: {
+          display: true,
+          text: titleText,
+          fontColor: '#000000',
+          fontSize: 23,
+          position: 'left',
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              fontColor: '#000000',
+              padding: 5,
+              fontSize: 13,
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            barThickness: 44,
+          }],
+          xAxes: [{
+            ticks: {
+              display: false,
+              beginAtZero: true,
+            },
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            minBarLength: 50,
+          }],
+        },
+        legend: {
+          display: false,
+        },
+        tooltips: {
+          enabled: false,
+        },
+      },
+    })
+  );
+};
+
--- src/views/statistics.js
+++ src/views/statistics.js
@@ -1,8 +1,5 @@
 import SmartView from './smart.js';
-import Chart from 'chart.js';
-import {OFFER_TITTLES, POINT_TYPES} from '../const.js';
-import ChartDataLabels from 'chartjs-plugin-datalabels';
-import {countPointsByType, getTotalPrice, makeItemsUniq, sortPriceByType} from '../utils/statistics';
+import {countPointsByType, getGraphChart, getTotalPrice, makeItemsUniq, sortPriceByType} from '../utils/statistics';
 
 const createStatisticsTemplate = () => (
   `<section class="statistics">
@@ -39,71 +36,14 @@ const renderMoneyChart = (moneyCtx, points) => {
     return arrPrice = arrPrice.concat([total]);
   });
 
-
-  return new Chart(moneyCtx, {
-    plugins: [ChartDataLabels],
-    type: 'horizontalBar',
-    data: {
-      labels: pointTypeUniqToUpperCase,
-      datasets: [{
-        data: arrPrice,
-        backgroundColor: '#ffffff',
-        hoverBackgroundColor: '#ffffff',
-        anchor: 'start',
-      }],
-    },
-    options: {
-      plugins: {
-        datalabels: {
-          font: {
-            size: 13,
-          },
-          color: '#000000',
-          anchor: 'end',
-          align: 'start',
-          formatter: (val) => `€ ${val}`,
-        },
-      },
-      title: {
-        display: true,
-        text: 'MONEY',
-        fontColor: '#000000',
-        fontSize: 23,
-        position: 'left',
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            fontColor: '#000000',
-            padding: 5,
-            fontSize: 13,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          barThickness: 44,
-        }],
-        xAxes: [{
-          ticks: {
-            display: false,
-            beginAtZero: true,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          minBarLength: 50,
-        }],
-      },
-      legend: {
-        display: false,
-      },
-      tooltips: {
-        enabled: false,
-      },
-    },
-  });
+  return getGraphChart(
+    moneyCtx,
+    pointTypeUniqToUpperCase,
+    arrPrice,
+    'MONEY',
+    '€',
+    true,
+  );
 };
 
 const renderTypeChart = (typeCtx, points) => {
@@ -112,76 +52,27 @@ const renderTypeChart = (typeCtx, points) => {
   const uniqTypesToUpperCase = uniqTypes.map((type) => type.toUpperCase());
   const pointsByTypeCounts = uniqTypes.map((type) => countPointsByType(points, type));
 
-  return new Chart(typeCtx, {
-    plugins: [ChartDataLabels],
-    type: 'horizontalBar',
-    data: {
-      labels: uniqTypesToUpperCase,
-      datasets: [{
-        data: pointsByTypeCounts,
-        backgroundColor: '#ffffff',
-        hoverBackgroundColor: '#ffffff',
-        anchor: 'start',
-      }],
-    },
-    options: {
-      plugins: {
-        datalabels: {
-          font: {
-            size: 13,
-          },
-          color: '#000000',
-          anchor: 'end',
-          align: 'start',
-          formatter: (val) => `${val}x`,
-        },
-      },
-      title: {
-        display: true,
-        text: 'TYPE',
-        fontColor: '#000000',
-        fontSize: 23,
-        position: 'left',
-      },
-      scales: {
-        yAxes: [{
-          ticks: {
-            fontColor: '#000000',
-            padding: 5,
-            fontSize: 13,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          barThickness: 44,
-        }],
-        xAxes: [{
-          ticks: {
-            display: false,
-            beginAtZero: true,
-          },
-          gridLines: {
-            display: false,
-            drawBorder: false,
-          },
-          minBarLength: 50,
-        }],
-      },
-      legend: {
-        display: false,
-      },
-      tooltips: {
-        enabled: false,
-      },
-    },
-  });
+  return getGraphChart(
+    typeCtx,
+    uniqTypesToUpperCase,
+    pointsByTypeCounts,
+    'TYPE',
+    'x',
+    false,
+  );
 };
 
 const renderTimeSpentChart = (timeSpendCtx, points) => {
-  // console.log('renderTimeSpentChart timeSpendCtx', timeSpendCtx);
-  // console.log('renderTimeSpentChart points', points);
   // Функция для отрисовки графика по датам
+
+  return getGraphChart(
+    timeSpendCtx,
+    ['A', 'B', 'C'],
+    [1,2,2],
+    'TIME-SPEND',
+    '',
+    false,
+  );
 };
 
 export default class Statistics extends SmartView {
@@ -200,7 +91,7 @@ export default class Statistics extends SmartView {
   removeElement() {
     super.removeElement();
 
-    if (this._moneyCart !== null || this._typeChart !== null || this._timeSpentChart !==null) {
+    if (this._moneyCart !== null || this._typeChart !== null || this._timeSpentChart !== null) {
       this._moneyCart = null;
       this._typeChart = null;
       this._timeSpentChart = null;
@@ -216,7 +107,7 @@ export default class Statistics extends SmartView {
   }
 
   _setCharts() {
-    if (this._moneyCart !== null || this._typeChart !== null || this._timeSpentChart !==null) {
+    if (this._moneyCart !== null || this._typeChart !== null || this._timeSpentChart !== null) {
       this._moneyCart = null;
       this._typeChart = null;
       this._timeSpentChart = null;
